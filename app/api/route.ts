48514bf3cdabc8d77e15c803550593611840b094
derek-li <the.derekli@gmail.com>
2024-05-08 14:19:26 -0700

    route.ts is not a module fix

--- app/api/route.ts
+++ app/api/route.ts
@@ -3,3 +3,8 @@
 // app/page.js	       app/route.js	     Conflict
 // app/page.js	       app/api/route.js	 Valid
 // app/[user]/page.js	 app/api/route.js	 Valid
+
+export const dynamic = 'force-dynamic' // defaults to auto
+export async function GET() {}
+// export async function GET(request: Request) {}
+
