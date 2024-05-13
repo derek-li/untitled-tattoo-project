6c5a47d378c57b507751b465b271418305218e2e
derek-li <the.derekli@gmail.com>
2024-05-13 13:28:51 -0700

    Cream color

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,16 @@
-'use client'
-
 import React from 'react'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        Hi
-        {/* <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
+    <main className="flex min-h-screen flex-col font-mono text-sm gap-4 p-24">
+      <p>Hi.</p>
+      <p>Welcome to untitled_tattoo_project.</p>
+      <p>If you&apos;re an artist, please sign up to start using this platform :)</p>
+      {/* Sign up flow */}
+      <p>If you&apos;re looking for a tattoo, stay tuned for exciting features.</p>
+      {/* <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+        
+        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
@@ -24,8 +27,8 @@ export default function Home() {
               priority
             />
           </a>
-        </div> */}
-      </div>
+        </div>
+      </div> */}
     </main>
   )
 }
